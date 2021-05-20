12c388020fd60821371300053ff0d777147ffae4
codefreeeze <codefreeeze@gmail.com>
2021-05-20 15:42:53 +0530

    code

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('nav-mode_switch').addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+
+    localStorage.setItem(
+      'theme',
+      document.body.classList.contains('dark') ? 'dark' : 'light'
+    );
+  });
+
+  if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark');
+  }
+});
